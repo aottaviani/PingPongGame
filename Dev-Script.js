02b7dd7d49b6d1de979b2d29d6bdfab82abbd055
aottaviani <a.ottaviani@hotmail.com>
2020-05-21 14:04:47 -0300

    Adicion de botones controladores y mov con mouse

--- Dev-Script.js
+++ Dev-Script.js
@@ -1,10 +1,40 @@
 var canvas = document.getElementById("myCanvasPaddle");
 var cxtx = canvas.getContext("2d");
+var pauseButton = document.getElementById("Start-Pause")
+var restartGame = document.getElementById("Reiniciar")
+var upLevel = document.getElementById("upLevel")
+var downLevel = document.getElementById("downLevel")
+var levelName = document.getElementById("levelTitle")
+var iconChange = document.getElementById("iconChange")
+var LevelBallSpeed = 5
+
+var level = 1
+
 var arrowUpPressed = false;
 var arrowDownPressed = false;
 
+document.addEventListener("mousemove",mouseMoveHandler, false);
 document.addEventListener("keydown", keyDownHandler, false)
 document.addEventListener("keyup", keyUpHandler, false)
+canvas.addEventListener("click",pauseWithMouse, false)
+pauseButton.addEventListener("click",pauseStart, false)
+restartGame.addEventListener("click", restartGameFunction,false)
+
+// upLevel.addEventListener("click", levelChange, false)
+// downLevel.addEventListener("click", levelChange ,false)
+
+function mouseMoveHandler(e){
+    var relativeY = e.clientY - canvas.offsetTop;
+    if(pauseButton == false){
+        if(relativeY > 0 && relativeY < user.height){
+            user.y = 0
+            } else if(relativeY < canvas.height && relativeY > canvas.height - 1*user.height/10){
+            user.y = canvas.height - user.height
+            }else if(relativeY > 0 && relativeY < canvas.height - 1*user.height/10){
+            user.y = relativeY - user.height;
+        }
+    }
+}
 
 
 function keyUpHandler(e){
@@ -13,6 +43,9 @@ function keyUpHandler(e){
     }
     else if(e.keyCode == 40){
         arrowDownPressed = false
+    }
+    if(e.keyCode == 13){
+        pauseStart();
     }
 }
 
@@ -23,6 +56,87 @@ function keyDownHandler(e){
    if(e.keyCode == 40){
        arrowDownPressed = true
    }
+}
+
+function pauseStart(){
+    if (pauseButton){
+        pauseButton = false
+     
+    }else{
+        pauseButton = true
+
+    }
+}
+
+function restartGameFunction() {
+    user.score = 0
+    user.y = (canvas.height/2) - 75/2
+    com.score = 0
+    com.y = (canvas.height/2) - 75/2;
+    pauseButton = true
+    resetBall();
+}
+
+function changeUpLevel(){
+    switch (level) {
+        case 1:
+            level++
+            levelName.innerHTML = "Level: Medium"
+            LevelBallSpeed = 6
+            break;
+        case 2:
+            level++
+            levelName.innerHTML = "Level: Hard"
+            LevelBallSpeed = 7
+            break;
+        case 3:
+            level++
+            levelName.innerHTML = "Level: Super Hard"
+            LevelBallSpeed = 8
+        break;
+        case 4:
+            level++
+            levelName.innerHTML = "Level: Monster!!!"
+            LevelBallSpeed = 10
+        break;
+        // case 5:
+        //     level++
+        //     levelName.innerHTML = "Level: Batata"
+        // break;                
+        default:
+            break;
+    }
+}
+
+function changeDownLevel(){
+    switch (level) {
+        case 2:
+            level--
+            levelName.innerHTML = "Level: Easy"
+            LevelBallSpeed = 5
+            break;
+        case 3:
+            level--
+            levelName.innerHTML = "Level: Medium"
+            LevelBallSpeed = 6
+            break;
+        case 4:
+            level--
+            levelName.innerHTML = "Level: Hard"
+            LevelBallSpeed = 7
+        break;
+        case 5:
+            level--
+            levelName.innerHTML = "Level: Super Hard!!!"
+            LevelBallSpeed = 8
+        break;
+        // case 5:
+        //     level++
+        //     levelName.innerHTML = "Level: Batata"
+        // break;                
+        default:
+            break;
+    }
 }
 
 
@@ -51,9 +165,15 @@ function drawScore() {
 
 // Paletas user y computer, y bola, son OBJETOS
 
-const user = {
+// const levels = {
+//     levelString: "easy",
+//     LevelBallSpeed: 5,
+//     computerLevel: 0.1,
+// }
+
+const com = {
     x:0,
-    y: (canvas.height/2) - 50/2,
+    y: (canvas.height/2) - 75/2,
     width: 20,
     height: 75,
     color: "Black",
@@ -61,22 +181,33 @@ const user = {
 
 }
 
-const com = {
+const user = {
     width: 20,
     height: 75,
     x: canvas.width - 20,
-    y: (canvas.height/2) - 50/2,
+    y: (canvas.height/2) - 75/2,
     color: "Black",
     score: 0,
 }
+
+// user.onclick = pauseWithMouse(event);
+
+function pauseWithMouse() {
+    if (pauseButton){
+        pauseButton = false
+     }else{
+        pauseButton = true
+    }
+}
+
 
 const ball = {
     x : canvas.width/2 ,
     y : canvas.height/2,
     r: 8,
-    speed: 5 ,
-    VelocityX: 5 ,
-    VelocityY:2 ,
+    speed: LevelBallSpeed ,
+    VelocityX: LevelBallSpeed * Math.cos(Math.PI/4) ,
+    VelocityY: LevelBallSpeed * Math.cos(Math.PI/4) ,
     color: "Black",
 }
 
@@ -92,18 +223,49 @@ const ball = {
 
 //Movimientos:
 
+function askLevelBallSpeed(){
+   
+    switch (level) {
+        case 1:
+            computerLevel = 0.1
+            LevelBallSpeed = 5
+            break;
+        case 2:
+            computerLevel = 0.2
+            LevelBallSpeed = 6
+            break;
+        case 3:
+            computerLevel = 0.3
+            LevelBallSpeed = 7
+            break;
+        case 4:
+            computerLevel = 0.4
+            LevelBallSpeed = 8
+            break;
+        case 5:
+            computerLevel = 0.5
+            LevelBallSpeed = 10
+        break;
+        default:
+            break;
+    }
+}
+
 function resetBall(){
     ball.x = canvas.width/2;
     ball.y = canvas.height/2;
-    ball.speed = 5;
+    ball.speed = LevelBallSpeed;
     ball.VelocityX = -ball.VelocityX;
+    user.y = canvas.height/2 - user.height/2
+    com.y = canvas.height/2 - com.height/2
 }
 
 
-let rectX = 0;
 
 function render(){
 //borro el contenido del canvas
+
+
 cxtx.clearRect(0, 0, canvas.width, canvas.height);
 
 //dibujo el score de cada participante
@@ -112,9 +274,9 @@ drawScore();
 //dibujo las paletas y la bola
 drawRect(user.x, user.y, user.width,user.height,user.color)
 drawRect(com.x, com.y, com.width,com.height,com.color)
-drawCircle(ball.x, ball.y, ball.r, ball.color)
 
-// rectX = rectX + 100;
+askLevelBallSpeed();
+drawCircle(ball.x, ball.y, ball.r, ball.color)
 
 }
 
@@ -130,89 +292,132 @@ function collision(b,p){
     p.left = p.x;
     p.right = p.x + p.width;
 
-
-
     return b.right > p.left && b.bottom > p.top && b.top < p.bottom && b.left < p.right;
 }
 
 
 
 function update(){
+    
     ball.x += ball.VelocityX
     ball.y += ball.VelocityY
     
-    if(ball.y + ball.r > canvas.height || ball.y - ball.r < 0){
+    if(ball.y + ball.r + ball.VelocityY > canvas.height){
+        ball.y = canvas.height - (ball.r*1.2)
         ball.VelocityY = - ball.VelocityY;
+
+    }else if(ball.y - ball.r - + ball.VelocityY < 0){
+        ball.y = (ball.r*1.2)
+        ball.VelocityY = - ball.VelocityY;
+
     }
 
-    let computerLevel = 0.02
+    // let computerLevel = 0.02
+        
+    var computerMove = (ball.y - (com.y + com.height/2)) * computerLevel;
 
-   
-     com.y += (ball.y - (com.y + com.height/2))* computerLevel;
-   
-    
+    if(com.y + computerMove > 0 && com.y + com.height + computerMove < canvas.height){
+        com.y += computerMove
+    }
 
-    let player = user
+    let player = com
     let direction = 1
      if(ball.x > canvas.width/2){
-       player = com;
+       player = user;
        direction = -1;
      } 
 
-   
-  if(collision(ball, player)){
-      
-    // collisionPoint es la posicion de la paleta donde pega la pelota
-    // es 0 si pega en el medio de la paleta, -height/2 de la paleta en esquina sup
-    // y height/2 de la paleta en esq inf
-
-        let collisionPoint = (ball.y - (player.y + player.height/2));
-    // Ahora normalizo collisionPoin entre -1 y 1:
-
-            collisionPoint = collisionPoint/(player.height/2);
     
-    //Y ahora ese punto entre -1 y 1 pasa a ser un ángulo entre -45 y 45
-    //pero en radianes (45° --> π/4)
 
-        let angleRad = (Math.PI/4)*collisionPoint;
+  if(collision(ball, player)){
+        // collisionPoint es la posicion de la paleta donde pega la pelota
+        // es 0 si pega en el medio de la paleta, -height/2 de la paleta en esquina sup
+        // y height/2 de la paleta en esq inf
+            askLevelBallSpeed();
+            ball.speed = LevelBallSpeed;
+            
+            let collisionPoint = (ball.y - (player.y + player.height/2));
+        // Ahora normalizo collisionPoin entre -1 y 1:
 
-        ball.VelocityX = direction * ball.speed * Math.cos(angleRad);
-        ball.VelocityY = ball.speed * Math.sin(angleRad);
+                collisionPoint = collisionPoint/(player.height/2);
         
-        ball.speed += 0,1;
+        //Y ahora ese punto entre -1 y 1 pasa a ser un ángulo entre -45 y 45
+        //pero en radianes (45° --> π/4)
 
+            let angleRad = (Math.PI/4)*collisionPoint;
+
+            ball.VelocityX = direction * ball.speed * Math.cos(angleRad);
+            ball.VelocityY = ball.speed * Math.sin(angleRad);
+            
+          
+
+            ball.speed += 0.1;
     }
     
     
-    // console.log(ball)
-    // console.log("b.right: "+ball.right+" > "+player.left) 
-    // console.log(player)
-    // console.log(player.left)
-    // console.log(ball.right)
-    // console.log(player.bottom)
-    // console.log(collision(ball,player))
 
     if(ball.x  < ball.r){
         com.score++;
         resetBall();
+        pauseButton = true;
     }else if( ball.x > canvas.width){
         user.score++
         resetBall();
+        pauseButton = true;
     }
 
     if(arrowUpPressed && user.y > 0){
-        user.y -= 5;
+        switch (level) {
+            case 1:
+                user.y -= 5;
+                break;
+            case 2:
+                user.y -= 8;
+                break;
+            case 3:
+                user.y -= 10;
+                break;          
+            default:
+                user.y -= 12
+                break;
+        }
+        
     }
     
     if(arrowDownPressed && user.y + user.height < canvas.height){
-        user.y += 5;
+        switch (level) {
+            case 1:
+                user.y += 5;
+                break;
+            case 2:
+                user.y += 8;
+                break;
+            case 3:
+                user.y += 10;
+                break;          
+            default:
+                user.y += 12
+                break;
+        }
     }
+
+    ball.x += ball.VelocityX
+    ball.y += ball.VelocityY
+    
 }
 
 function game(){
+            
+    if(pauseButton){
+    //askLevelBallSpeed()
+    render();
+    }else{
+    // askLevelBallSpeed();
     render(); //dibuja
     update(); //Para controlar movimientos, colisiones, act. de score, etc.
-  
+    }
+    // console.log(ball.speed)
+    // console.log(LevelBallSpeed)
 }
 
 const framePerSecond = 50;
